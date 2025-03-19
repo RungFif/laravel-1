0ff8fce4cbb1fa364453a7a42a0f8d488dc2dc31
RungFif <rafifpranaja301@gmail.com>
2025-03-19 08:27:33 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//get data
+Route::get('/arkandi', function () {
+    $penampung = 'Hello World';
+    return view('arkandi', ['penampung' => $penampung]);
+});
