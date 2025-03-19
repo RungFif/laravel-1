7548d0d90d125916a77e05c2b45cd50e2f06b9a6
RungFif <rafifpranaja301@gmail.com>
2025-03-19 08:51:57 +0700

    changes

--- database/migrations/2025_03_19_013015_create_school_table.php
+++ database/migrations/2025_03_19_013015_create_school_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('schools', function (Blueprint $table) {
+        Schema::create('school', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 100);
+            
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('schools');
+        Schema::dropIfExists('school');
     }
 };
